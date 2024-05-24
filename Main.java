6b5789b5a78b6f6488922b2776620d9871238d52
Fushiftstudio <131993090+Fushiftstudio@users.noreply.github.com>
2024-05-24 12:42:49 +0200

    Update Main.java
    delete small prin 
    update 24052412DO

--- Main.java
+++ Main.java
@@ -3,28 +3,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Vytvoření Scanneru pro čtení vstupu z konzole
+      
         Scanner sc = new Scanner(System.in);
 
-        // Zadání délky použitého filamentu v centimetrech
+      
         System.out.println("Zadejte délku použitého filamentu v centimetrech:");
         double delka = sc.nextDouble();
 
-        // Zadání hustoty filamentu v gramech na metr
+      
         System.out.println("Zadejte hustotu filamentu v gramech na metr:");
         double hustota = sc.nextDouble();
 
-        // Výpočet hmotnosti
+      
         double hmotnost = delka * hustota;
 
-        // Zadání ceny jednoho filamentu
+        
         System.out.println("Zadejte cenu jednoho filamentu v Kč:");
         double cenaFilamentu = sc.nextDouble();
 
-        // Výpočet ceny výrobku
+      
         double cenaVyrobku = hmotnost / 1000 * cenaFilamentu;
 
-        // Vypsání výsledků
+        
         System.out.println("Hmotnost výrobku je " + hmotnost + " gramů.");
         System.out.println("Cena výrobku je " + cenaVyrobku + " Kč.");
     }
